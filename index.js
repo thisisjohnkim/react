7d61deecac8014194e9cfc9db11361db51c636bd
jkim <jkim@fonteva.com>
2018-08-27 01:17:01 -0400

    save

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express  = require('express');
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientId: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
