5ab56c3b0823202d47772b1bbc61b7b05344e1d4
jkim <jkim@fonteva.com>
2018-09-04 15:11:58 -0400

    save

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.COOKIE_URI,
   cookieKey: process.env.COOKIE_KEY
 };
