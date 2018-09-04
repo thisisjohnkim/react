ce5200691fd8a6ce74d0033047423e7d0062dc74
jkim <jkim@fonteva.com>
2018-09-04 17:07:49 -0400

    updates with prettier

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.COOKIE_URI,
-  cookieKey: process.env.COOKIE_KEY
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.COOKIE_URI,
+    cookieKey: process.env.COOKIE_KEY
 };
